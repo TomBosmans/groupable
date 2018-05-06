a37471cc8ef7e64294740466f354b7739debfb8e
TomBosmans <t.bosse@hotmail.com>
2018-05-06 15:03:42 +0200

    correctly link to gem app/models/concerns/groupable

--- lib/groupable/engine.rb
+++ lib/groupable/engine.rb
@@ -1,4 +1,6 @@
-require './app/models/concerns/groupable'
+spec = Gem::Specification.find_by_name("groupable")
+gem_root = spec.gem_dir
+require "#{gem_root}/app/models/concerns/groupable"
 
 module Groupable
   class Engine < ::Rails::Engine
