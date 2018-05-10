e1b3e70f6b47d9f21478714856c0380f71750916
TomBosmans <t.bosse@hotmail.com>
2018-05-10 14:38:29 +0200

    fix the scopes so it also works on group itself

--- app/models/concerns/groupable.rb
+++ app/models/concerns/groupable.rb
@@ -5,9 +5,12 @@ module Groupable
     has_one :group_item, as: :groupable, dependent: :destroy
     has_one :group, through: :group_item
 
-    scope :where_group_id,
-          ->(id) { joins(:group).where(groups: { id: id }) }
-    scope :where_group_name,
-          ->(name) { joins(:group).where(groups: { name: name }) }
+    scope :where_group_id, ->(id) do
+      joins(:group_item).where(group_items: { group_id: id })
+    end
+
+    scope :where_group_name, ->(name) do
+      joins(:group_item, :group).where(group_items: { groups: { name: name } })
+    end
   end
 end
