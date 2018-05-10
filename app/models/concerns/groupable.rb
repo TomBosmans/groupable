90ff6d4529c46754385fe71d97be3d690291f141
TomBosmans <t.bosse@hotmail.com>
2018-05-10 14:00:20 +0200

    destroy group item if the item is destroyed

--- app/models/concerns/groupable.rb
+++ app/models/concerns/groupable.rb
@@ -2,7 +2,7 @@ module Groupable
   extend ActiveSupport::Concern
 
   included do
-    has_one :group_item, as: :groupable
+    has_one :group_item, as: :groupable, dependent: :destroy
     has_one :group, through: :group_item
 
     scope :where_group, ->(group_id) { joins(:group_item).where(group_items: { group_id: group_id }) }
